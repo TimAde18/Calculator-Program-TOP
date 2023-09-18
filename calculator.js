1efc144f2ed014dcd79832e4fef8b5472f362856
TimAde18 <aoadewole18@gmail.com>
2023-09-18 13:35:09 -0500

    first commit

---
+++ calculator.js
@@ -0,0 +1,19 @@
+let firstNumber = 0;
+let operator;
+let secondNumber = 0;
+
+function operate(firstNumber, operator, secondNumber) {
+    return firstNumber + operator + secondNumber;
+} 
+
+function add(num1, num2){
+    return num1 + num2;
+};
+
+function subtract(num1, num2) {
+    return num1 - num2;
+};
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
